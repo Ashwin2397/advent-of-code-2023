335377156d9cc0254c230c64e5f123c29b2e1e5e
Ashwin2397 <ashwin.selvakumar@lendesk.com>
2023-12-01 13:56:58 -0800

    p2 sln, messy

--- day1.rb
+++ day1.rb
@@ -1,7 +1,37 @@
 # line: 51591twosix4dhsxvgghxq
+
 def find_code(line)
-  nums = line.each_char.filter { |c| is_num?(c) }
+  max_size = numbers.keys.map(&:size).max
+  nums = []
+  i = 0
+  line.each_char.with_index do |char, i|
+    char = line[i]
+    
+    if is_num?(char)
+      nums.append(char)
+      i += 1
+      next
+    end
+    
+    chunk = line.slice(i, max_size)
+    number_string = numbers.keys.find { |number| chunk.start_with?(number) }
+
+    if number_string
+      nums.append(numbers[number_string]) 
+      i += number_string.size
+      next
+    end
+
+    i += 1
+  end
+
   (nums.first + nums.last).to_i
+end
+
+def numbers
+  @numbers ||= {
+    'one' => '1', 'two' => '2', 'three' => '3', 'four' => '4', 'five' => '5', 'six' => '6', 'seven' => '7', 'eight' => '8', 'nine'  => '9'
+  }
 end
 
 def is_num?(char)
