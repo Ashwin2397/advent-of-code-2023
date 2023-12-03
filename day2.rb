b08e50c1c7b2100378542dbb376eb57dc8852ce8
Ashwin2397 <ashwin.selvakumar@lendesk.com>
2023-12-02 17:36:05 -0800

    Day 2; P2 Complete

--- day2.rb
+++ day2.rb
@@ -1,22 +1,33 @@
 # 12 red, 13 green, 14 blue
 
-def max_value(line, color)
+def max_or_min_value(line, color)
   line.scan(/([0-9][0-9]?) #{color}/).map(&:first).map(&:to_i).max
 end
 
 def is_valid?(line)
-  max_value(line, 'red') <= 12 &&
-  max_value(line, 'green') <= 13 &&
-  max_value(line, 'blue') <= 14
+  max_or_min_value(line, 'red') <= 12 &&
+  max_or_min_value(line, 'green') <= 13 &&
+  max_or_min_value(line, 'blue') <= 14
+end
+
+def power(line)
+  max_or_min_value(line, 'red') *
+  max_or_min_value(line, 'green') *
+  max_or_min_value(line, 'blue')
 end
 
 valid = 0
+power_sum = 0
 File.open('day2.txt') do |file|
   file.each_line do |line|
+    # P1
     game_id = /Game (.*):/.match(line).to_a.last.to_i
-
     valid += game_id if is_valid?(line)
+
+    # P2
+    power_sum += power(line)
   end
 end
 
 puts valid
+puts power_sum
